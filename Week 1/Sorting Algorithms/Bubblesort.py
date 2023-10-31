d8b5d73565f84529e1bfaac440a6c89601138d4f
subash <subashkj005@gmail.com>
2023-10-31 18:06:05 +0530

    bubble sort

---
+++ Week 1/Sorting Algorithms/Bubblesort.py
@@ -0,0 +1,26 @@
+arr = [3, 5, 3, 6, 7, 83, 4, 2, 6]
+
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        swapped = False
+        for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+                swapped = True
+        if not swapped:
+            break
+
+    print(arr)
+
+
+bubble_sort(arr)
+
+"""
+Complexity of Bubble Sort:-
+
+Worst case : O(n^2) 
+Best case : O(n) 
+Space complexity : O(1)
+
+"""
